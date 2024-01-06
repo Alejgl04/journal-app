c82602d4283cc702f0c1c81ac3a12342d59a935c
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-01-06 16:00:53 -0500

    navbar drawer

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,10 @@
 import { Typography } from '@mui/material'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h3'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>Quis id mollit qui aliquip. Id eiusmod ex tempor voluptate irure eu dolore pariatur laborum. Veniam dolore nulla pariatur anim culpa nisi veniam. Consectetur exercitation consequat et adipisicing Lorem qui reprehenderit laborum. Ipsum dolor laborum dolore voluptate esse. Labore duis non nisi esse in proident.</Typography>
+    </JournalLayout>
   )
 }
