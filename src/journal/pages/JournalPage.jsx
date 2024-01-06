50b25459a17650a615948776fae2328d81d172bb
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-01-06 16:36:01 -0500

    imageGallery

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,14 @@
 import { Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
-import { NothingSelectedView } from '../views/'
+import { NoteView, NothingSelectedView } from '../views/'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>Quis id mollit qui aliquip. Id eiusmod ex tempor voluptate irure eu dolore pariatur laborum. Veniam dolore nulla pariatur anim culpa nisi veniam. Consectetur exercitation consequat et adipisicing Lorem qui reprehenderit laborum. Ipsum dolor laborum dolore voluptate esse. Labore duis non nisi esse in proident.</Typography> */}
 
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
+      <NoteView />
 
     </JournalLayout>
   )
