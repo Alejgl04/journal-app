de7bdf663af5a13b15e013bf0dee5062631fab67
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-01-06 14:47:19 -0500

    apptheme

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,9 @@
+import { Typography } from '@mui/material'
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <>
+      <Typography variant='h3'>JournalPage</Typography>
+    </>
   )
 }
