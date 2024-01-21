013ca82ddef9f262d50a27f7d4c946f0ec07e7be
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-01-21 15:03:15 -0500

    firebase app

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore/lite';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDx0n9PnhKO6MK8FIbP7-l22DtYJA_EK3U",
+  authDomain: "react-course-153f1.firebaseapp.com",
+  projectId: "react-course-153f1",
+  storageBucket: "react-course-153f1.appspot.com",
+  messagingSenderId: "1020685827022",
+  appId: "1:1020685827022:web:2ca52f1a1b45455d51899f"
+};
+
+// Initialize Firebase
+export const firebaseApp  = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth( firebaseApp );
+export const firebaseDb   = getFirestore( firebaseApp );
