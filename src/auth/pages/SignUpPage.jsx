8f342210c6413381578a183bc47e72424c52d9d5
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-01-25 22:07:49 -0500

    login with credentials

--- src/auth/pages/SignUpPage.jsx
+++ src/auth/pages/SignUpPage.jsx
@@ -1,27 +1,93 @@
+import { useState } from 'react';
 import {  Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks/useForm';
+import { useDispatch } from 'react-redux';
+import { startCreateUserCredentials } from '../../store/auth';
+
+const formData = {
+  email: '',
+  password: '',
+  displayName: ''
+}
+
+const formValidations = {
+  email: [ ( value ) => value.includes('@'), 'Email should have @'],
+  password: [ ( value ) => value.length >= 1, 'Password should at least 6 characters'],
+  displayName: [ ( value ) => value.length >= 1, 'Name is required'],
+
+}
 
 export const SignUpPage = () => {
+  
+  const dispatch = useDispatch();
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const { 
+    isFormValid, displayName, email, password, onInputChange, formState,
+    displayNameValid, emailValid, passwordValid, 
+  } = useForm(formData, formValidations);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setFormSubmitted(true);
+    if (!isFormValid) return; 
+    
+    dispatch(startCreateUserCredentials(formState));
+    
+  }
+
   return (
     // Container
     <AuthLayout title="Sign up">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
 
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label="Full Name" type="text" fullWidth></TextField>
+            <TextField 
+              label="Full Name" 
+              type="text" 
+              fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
+              error={ !!displayNameValid && formSubmitted }
+              helperText={ displayNameValid }
+            ></TextField>
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label="Email" type="email" fullWidth></TextField>
+            <TextField 
+              label="Email" 
+              type="email" 
+              fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
+              error={ !!emailValid && formSubmitted }
+              helperText={ emailValid }
+            ></TextField>
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label="Password" type="password" fullWidth></TextField>
+            <TextField 
+              label="Password" 
+              type="password" 
+              fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
+              error={ !!passwordValid && formSubmitted }
+              helperText={ passwordValid }
+            ></TextField>
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12}>
-              <Button variant="contained" fullWidth>
+              <Button 
+                variant="contained" 
+                fullWidth
+                type="submit"
+              >
                 Submit
               </Button>
             </Grid>
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,4 @@
-import { signInWithGoole } from '../../firebase/providers';
+import { registerUserWithCredentials, signInWithGoole } from '../../firebase/providers';
 import { checkingCredentials, login, logout } from './authSlice';
 
 export const checkingAuthentication = ( email, password ) => {
@@ -18,5 +18,23 @@ export const startGoogleSignIn = () => {
 
     dispatch( login( result ) )
   }
+}
+
+export const startCreateUserCredentials = ({ email, password, displayName }) => {
+
+  return async( dispatch ) => {
+    dispatch( checkingCredentials() );
+    
+    const {ok, uid, photoURL, errorMessage } = await registerUserWithCredentials({email, password, displayName });
+    if ( !ok ) return dispatch( logout ({errorMessage }));
+
+    dispatch( login({
+      uid,
+      photoURL,
+      displayName,
+      email,
+    }));
+  }
+
 }
 
