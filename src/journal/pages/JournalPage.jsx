b292b7a1e1b43a0a163bcf48814e511288984b51
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-02-06 18:07:11 -0500

    journal Slice methods

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -8,8 +8,8 @@ export const JournalPage = () => {
     <JournalLayout>
       {/* <Typography>Quis id mollit qui aliquip. Id eiusmod ex tempor voluptate irure eu dolore pariatur laborum. Veniam dolore nulla pariatur anim culpa nisi veniam. Consectetur exercitation consequat et adipisicing Lorem qui reprehenderit laborum. Ipsum dolor laborum dolore voluptate esse. Labore duis non nisi esse in proident.</Typography> */}
 
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
 
       <IconButton
         size="large"
---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const journalSlice = createSlice({
+  name: 'journal',
+  initialState: {
+    isSaving: true,
+    messageSaved: '',
+    notes: [],
+    // active: null,
+    active: {
+      id: 'abc123',
+      title: '',
+      body: '',
+      date: 123123,
+      imagesUrls: [],
+    }
+  },
+  reducers: {
+    addNewEmptyNote: (state, action ) => {
+
+    },
+    
+    setActiveNote: ( state, action ) => {
+
+    },
+
+    setNotes: ( state, action ) => {
+
+    },
+    
+    setSaving: ( state ) => {
+
+    },
+    
+    updateNote: ( state, action ) => {
+
+    },
+
+    deleteNoteById: ( state, action ) => {
+
+    }
+
+  },
+});
+
+  // Action creators are generated for each case reducer function
+export const { 
+  addNewEmptyNote,
+  setActiveNote,
+  setNotes,
+  setSaving,
+  updateNote,
+  deleteNoteById
+} = journalSlice.actions;
